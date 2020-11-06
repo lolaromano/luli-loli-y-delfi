6cae285ddd2e56519ea368a1a53530c8aa430bc0
Delfina Cardona <dcardona@udesa.edu.ar>
2020-11-06 10:31:23 -0300

    agregué algo de uikit a la pagina de generos. falta cambiar las fotos
    hice algo en java pero no lo puse ahí tvia porque no está terminado. después lo pongo

--- js/generos.js
+++ js/generos.js
@@ -1,19 +1,21 @@
 window.addEventListener('load',function(){
 
-fetch (`https://api.themoviedb.org/3/movie/550?api_key=a7861c4f6175fa7e9d7361d67809cb5a`)
-.then(function (response) {
-    return response.json()
-})
-.then(function (data) {
-    console.log(data);
+    var apiKey = 'a7861c4f6175fa7e9d7361d67809cb5a'
 
-    data.results.forEach(actor => {
-        document.querySelector ('').innerHTML += //(aca esta el error, falta agregar algo)
-        
-    });
-})
-.catch(function (error){
-    console.log('El error fue: ' +error);
-})
-
-})
+    fetch (`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        //FALTA LLENAR ACA
+    })
+    .catch(function (error){
+        console.log('El error fue: '+error);
+    })
+    
+    
+    
+    
+    
+    
+    })
