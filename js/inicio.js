37720c9e86495236be31255e692a7e13730bb656
lolaromano <lromano@udesa.edu.ar>
2020-11-08 23:25:00 -0300

    inicio java

---
+++ js/inicio.js
@@ -0,0 +1,64 @@
+window.addEventListener('load',function(){
+
+    var apiKey = '1dd189fc2111fcf3d905a59b0cd42a3c'
+
+    
+    fetch (`https://api.themoviedb.org/3/movie/latest?api_key=${apiKey}&language=en-US`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (resultados) {
+        console.log (resultados.data);
+
+        for (let index = 0; index < resultados.data.length; index++) {
+            const element = resultados.data [index];
+            
+        }
+    })
+    .catch(function (error){
+        console.log('El error fue: '+error);
+    })
+
+
+    fetch (`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (resultados) {
+        console.log (resultados.data);
+    })
+    .catch(function (error){
+        console.log('El error fue: '+error);
+    })
+    
+ 
+    
+    fetch (`https://api.themoviedb.org/3/tv/{tv_id}?api_key=${apiKey}&language=en-US`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (resultados) {
+        console.log (resultados.data);
+    })
+    .catch(function (error){
+        console.log('El error fue: '+error);
+    })
+
+
+    fetch (`https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (resultados) {
+        console.log (resultados.data);
+    })
+    .catch(function (error){
+        console.log('El error fue: '+error);
+    })
+    
+    
+    
+    
+    
+    
+    })
