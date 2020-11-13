040234fd01c4245f05454468d41a824f651993dd
Luisa Nazar <luisanazar@MacBook-Pro-de-Luisa-2.local>
2020-11-13 09:17:53 -0300

    correccion con martin

--- js/inicio.js
+++ js/inicio.js
@@ -2,13 +2,13 @@ window.addEventListener('load',function(){
 
     var apiKey = '1dd189fc2111fcf3d905a59b0cd42a3c'
 
-    
+    //fetch("https://api.themoviedb.org/3/movie/550?api_key=a30b09d876844ad1c87bdc1437f89d73")
     fetch (`https://api.themoviedb.org/3/movie/latest?api_key=${apiKey}&language=en-US`)
     .then(function (response) {
         return response.json()
     })
     .then(function (resultados) {
-        console.log (resultados.data);
+        console.log (resultados);
 
         for (let index = 0; index < resultados.data.length; index++) {
             const element = resultados.data [index];
@@ -19,7 +19,7 @@ window.addEventListener('load',function(){
         console.log('El error fue: '+error);
     })
 
-
+/*
     fetch (`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
     .then(function (response) {
         return response.json()
@@ -56,7 +56,7 @@ window.addEventListener('load',function(){
         console.log('El error fue: '+error);
     })
     
-    
+    */
     
     
     
