7f17eaa9f37e6502998328783f2433e4c70b255a
lolaromano <lromano@udesa.edu.ar>
2020-11-20 13:17:28 -0300

    detalles de las imagenes

--- js/inicio.js
+++ js/inicio.js
@@ -3,7 +3,6 @@ window.addEventListener('load',function(){
     var apiKey = '1dd189fc2111fcf3d905a59b0cd42a3c'
     var linkImagen = "https://image.tmdb.org/t/p/w500"
 
-    console.log (linkImagen);
     
     fetch (`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
         .then(function (response) {
@@ -18,12 +17,30 @@ window.addEventListener('load',function(){
                 const element = data.results [index];
                 
                 ul.innerHTML += `
-                <li>
-                    <div class="uk-panel"> 
-                        <img src="${linkImagen}${element.poster_path}" alt="">
-                        <div class="uk-position-center uk-panel"><h1></h1></div>
-                    </div>
-                </li>
+                <div class="uk-child-width-1-2@m" uk-grid>
+                    <li>
+                        <div class="uk-inline">
+                            <div class="uk-panel"> 
+                                <img src="${linkImagen}${element.poster_path}" alt="">
+                                <div class="uk-position-center uk-panel"><h1></h1></div>
+                                <div class="uk-overlay uk-overlay-primary uk-position-bottom">
+                                <div class="uk-width-expand">                            
+                                <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">                                
+                                <li><a href="#">${element.release_date}</a></li>                                                             
+                                <li><a href="#">${element.popularity}</a></li>                            
+                                </ul>                        
+                            </div>
+                        </div>
+                    </li>
+                </div>
+              
+            
+                    
+                    
+                        
+                    
+            
+             
         
                 `;
 
