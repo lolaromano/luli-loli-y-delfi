3a068c14262e50a72c2ab84cdb880b553ae0ef3e
Delfina Cardona <dcardona@udesa.edu.ar>
2020-11-25 13:45:02 -0300

    avances busqueda

--- js/resultados.js
+++ js/resultados.js
@@ -3,14 +3,15 @@ window.addEventListener('load',function() {
     var apiKey = '1dd189fc2111fcf3d905a59b0cd42a3c';
     var linkImagen = "https://image.tmdb.org/t/p/w500";
 
-
     this.location.search;
-    var queryString = new URLSearchParams(queryString);
-    var queryString = location.search;
-    
 
-    var buscador = document.querySelector (".buscador");
-    var buscando = queryString.get('buscador');
+    var queryString = location.search;
+    var queryStringObj = new URLSearchParams(queryString);
+
+    var buscadorPelis = document.querySelector (".resultadosPeliculas");
+    var buscadorSeries = document.querySelector (".resultadosSeries");
+
+    var buscando = queryStringObj.get("search");
 
 ///////nuevo
 
@@ -23,50 +24,43 @@ fetch (`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-
     .then(function (data) {
         console.log (data);
       
-        var ul = document.querySelector(".resultadospeliculas");
-        var resultadoBusqueda = data.results;
-        
+        for (let i = 0; i < data.results.length; i++) {
+            const element = data.results[i];
+            buscadorPelis.innerHTML +=
+            `<li>
+                <a class="buscador" href="detalle.html?type=${element.id}>${element.title}</a>
+                </li>
+            <img src="${linkImagen}${element.poster_path}" alt="">`
+        }
+    })
 
-        resultadoBusqueda.forEach(function(element){
-            ul.innerHTML +=
-            `<li> <a class="buscador" href="detalle.html?type=${element.id}>${element.title}</a></li>`
-             
-        })
 
     .catch(function (error){
         console.log('El error fue: '+error);
         
     })
 
-})
 
 fetch (`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${buscando}}&page=1&include_adult=false`)
     .then(function (response) {
         return response.json()
     })
     
-    .then(function (data){
+    .then(function (data) {
         console.log (data);
-        
-        var ul = document.querySelector(".resultadosseries");
-        var resultadoBusqueda = data.results;
-        
-
-        resultadoBusqueda.forEach(function(element){
-            ul.innerHTML +=
-            `<li> <a class="buscador" href="detalle.html?type=${element.id}>${element.title}</a></li>`
-
-            console.log(data);
-                
-        });
-        
+      
+        for (let i = 0; i < data.results.length; i++) {
+            const element = data.results[i];
+            buscadorSeries.innerHTML +=
+            `<li> <a class="buscador" href="detalle.html?type=${element.id}>${element.title}</a></li>
+            <img src="${linkImagen}${element.poster_path}" alt="">`
+        }
     })
 
     .catch(function (error){
     console.log('El error fue: '+error);
 
     })
-
 
 
 }); //fin
